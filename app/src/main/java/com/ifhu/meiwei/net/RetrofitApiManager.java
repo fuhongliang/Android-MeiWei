0562c3c6eb02e67bf197d8bbea2b8d7427411e5c
fuhl <814073395@qq.com>
2019-05-17 11:40:14 +0800

    [ADDED]完成多渠道打包配置

--- app/src/main/java/com/ifhu/meiwei/net/RetrofitApiManager.java
+++ app/src/main/java/com/ifhu/meiwei/net/RetrofitApiManager.java
@@ -1,5 +1,7 @@
 package com.ifhu.meiwei.net;
 
+import com.ifhu.meiwei.BuildConfig;
+
 /**
  *
  * @author KevinFu
@@ -7,5 +9,23 @@ package com.ifhu.meiwei.net;
  * Copyright (c) 2019 KevinFu
  */
 public class RetrofitApiManager {
+
+    /**
+     * 正式环境
+     */
+    private static String RELEASE_URL = "http://正式url";
+    /**
+     * 预发布环境
+     */
+    private static String TEST_URL = "http://测试url";
+    /**
+     * 开发环境
+     */
+    private static String DEV_URL = "http://开发url";
+
+    private static final String STRING_API_ENV = BuildConfig.STRING_API_ENV;
+
+    private static String BASE_URL = STRING_API_ENV.equals("0")
+            ? RELEASE_URL : (STRING_API_ENV.equals("1") ? TEST_URL : DEV_URL);
 
 }
